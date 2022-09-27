1777f03e8c064a3d6b78aaaa0326ab192fffcd66
ArchAL85 <tilicheev@innolyceum.ru>
2022-09-27 10:16:26 +0300

    Upload DB

--- database/crud.py
+++ database/crud.py
@@ -67,23 +67,63 @@ def load_schedule():
     """Выполняется только для загрузки или обновления расписания"""
     session = SessionLocal()
     school = EduTatar(ADMIN)
+    add_lessons = []
     if school.connected()[0] == 'Ok':
         schedule = school.get_schedule()
         for class_ in schedule.keys():
-            print(schedule[class_])
             for day_of_week, lessons in schedule[class_].items():
-                print(day_of_week, lessons)
-                add_lessons = [
-                    Schedule(
-                        lesson=lessons[i][0],
-                        day_of_week=day_of_week,
-                        index_number=i,
-                        teacher_id=get_user_by_name(lessons[i][1]).edu_tatar_id,
-                        class_id=class_
-                    ) for i in range(len(lessons))
-                ]
-                session.add_all(add_lessons)
+                for i in range(len(lessons)):
+                    user = get_user_by_name(lessons[i][1])
+                    if type(user) == Users:
+                        lesson = Schedule(
+                            lesson=lessons[i][0],
+                            day_of_week=day_of_week,
+                            index_number=i,
+                            teacher_id=user.edu_tatar_id,
+                            class_id=class_
+                            )
+                    else:
+                        for u in user:
+                            lesson = Schedule(
+                                lesson=lessons[i][0],
+                                day_of_week=day_of_week,
+                                index_number=i,
+                                teacher_id=u.edu_tatar_id,
+                                class_id=class_
+                            )
+                    add_lessons.append(lesson)
+        session.add_all(add_lessons)
         session.commit()
+
+
+def set_student_role():
+    school = EduTatar(ADMIN)
+    students = school.get_students()
+    session = SessionLocal()
+    for class_, students in students.items():
+        add_role = [
+            User_role(
+                user_id=get_user(edu_id=student_id).user_id,
+                role_id=8
+            ) for student_id in students.keys()
+        ]
+        session.add_all(add_role)
+    session.commit()
+
+
+def set_student_class():
+    school = EduTatar(ADMIN)
+    students = school.get_students()
+    session = SessionLocal()
+    for class_, students in students.items():
+        add_class = [
+            User_class(
+                user_id=get_user(edu_id=student_id).user_id,
+                class_id=get_class_id(class_)
+            ) for student_id in students.keys()
+        ]
+        session.add_all(add_class)
+    session.commit()
 
 
 def get_user_by_name(name: str):
@@ -93,6 +133,45 @@ def get_user_by_name(name: str):
         return session.query(Users).filter(Users.surname == name[0], Users.name == name[1]).first()
     if len(name) == 3:
         return session.query(Users).filter(Users.surname == name[0], Users.name == name[1], Users.middlename == name[2]).first()
+    if len(name) > 3:
+        return session.query(Users).filter(
+            or_(and_(Users.surname == name[0], Users.name == name[1], Users.middlename == name[2]),
+                and_(Users.surname == name[0], Users.name == name[1], Users.middlename == name[2]))).all()
 
 
-load_schedule()
+def get_students_list(class_id):
+    school = EduTatar(ADMIN)
+    students = school.get_students()
+    session = SessionLocal()
+    for class_, students in students.items():
+        pass
+    session.commit()
+
+
+def get_user(surname=None, name=None, middlename=None, edu_id=None, telegram_id=None, user_id=None):
+    """По указанному набору данных возвращает пользователя.
+    Для edu_tatar_id, user_id и telegram_id возвращается 1 пользователь"""
+
+    session = SessionLocal()
+    user = session.query(Users)
+    if edu_id:
+        return user.filter(Users.edu_tatar_id == edu_id).first()
+    if surname and name and middlename:
+        return user.filter(Users.surname == surname, Users.name == name, Users.middlename == middlename).all()
+    if surname and name:
+        return user.filter(Users.surname == surname, Users.name == name).all()
+    if telegram_id:
+        return user.filter(Users.telegram_id == telegram_id).first()
+    if user_id:
+        return user.filter(Users.user_id == user_id).first()
+
+
+def get_class_id(class_: str):
+    """Получаем ID класса, по его обозначению"""
+    number = class_[:-1]
+    literal = class_[-1]
+    session = SessionLocal()
+    return session.query(Classes).filter(Classes.number == number, Classes.literal == literal).first().class_id
+
+
+set_student_class()
